15cf18ca4f6b619c3b739b58261cce7ae2c97397
dmutokhlyaev <60260395+dmutokhlyaev@users.noreply.github.com>
2020-01-29 17:36:10 +0500

    Add files via upload

--- GSM/js/common.js
+++ GSM/js/common.js
@@ -1,3 +1,11 @@
 $(function(){
+	$('.open-additional-fields label').click(function() {
+		if($('.open-additional-fields input').is(':checked')){
+			$('.additional-fields').removeClass('open');
+		} else {
+			$('.additional-fields').addClass('open');
+		}
+	})
 
+	$('.select').selectize({});
 });
