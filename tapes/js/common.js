fb0e517568d3910ceed8284d1c067f128b7d6ce1
dmutokhlyaev <60260395+dmutokhlyaev@users.noreply.github.com>
2020-02-05 21:24:48 +0500

    Add files via upload

--- tapes/js/common.js
+++ tapes/js/common.js
@@ -1,10 +1,12 @@
 $(function(){
+	/* Главное меню на малых экранах */
+
 	$('.hamburger').click(function() {
 
 		if ( $(this).hasClass('is-active') ) {
 
 			$(this).removeClass('is-active');
-			$('.menu').height( 0 );
+			$('.menu').removeAttr('style');
 
 		} else {
 
@@ -13,5 +15,27 @@ $(function(){
 			$('.menu').height( $('.menu__list').innerHeight() );
 		}
 
+	});
+
+	/* Скролл до секции */
+
+	$("a[href^='#']").click(function(){
+            var _href = $(this).attr("href");
+
+            $('.hamburger').removeClass('is-active');
+			$('.menu').removeAttr('style');
+
+            $("html, body").animate({scrollTop: $(_href).offset().top -60 + "px"}, 600);
+            return false;
+    });
+
+    /* Попап*/
+
+	$('.home__button').click(function() {
+		$('.popup').addClass('popup_active');
+	});
+
+	$('.popup__close').click(function() {
+		$('.popup').removeClass('popup_active');
 	});
 });
