f2d163a15d11b14ccd3019f938e7b10ad1723ea6
dmutokhlyaev <60260395+dmutokhlyaev@users.noreply.github.com>
2020-02-21 20:06:06 +0500

    Add files via upload

---
+++ browshop/js/common.js
@@ -0,0 +1,43 @@
+$(function(){
+
+	$('.home-slider').slick({
+		prevArrow: $('.home-slider__prev'),
+		nextArrow: $('.home-slider__next'),
+	});
+
+	$('.home-slider').on('beforeChange', function(event, slick, currentSlide, nextSlider) {
+		var sliderCount = '0' + (nextSlider + 1) + '/03'
+
+		$('.home-slider__count').text(sliderCount);
+	});
+
+
+	$('.home-slider').on('setPosition', function() {
+
+		$('.home-slider__item').height( $('.home').height() );
+	
+	});
+
+	$('.button_search').click( function() {
+
+		$('.main-menu').toggleClass('close');
+		$('.search').toggleClass('close');
+
+		return false;
+	});
+
+	$('.hamburger').click( function() {
+
+		if ( $('.hamburger').hasClass('is-active') ) {
+
+			$('.hamburger').removeClass('is-active');
+			$('.main-menu').removeAttr('style');
+
+		} else {
+
+			$('.hamburger').addClass('is-active');
+			$('.main-menu').height( $('.main-menu ul').height() );
+
+		}
+	})
+});
