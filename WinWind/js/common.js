7bb24592fdead663a5f58e64cd8318c3b439f1db
dmutokhlyaev <60260395+dmutokhlyaev@users.noreply.github.com>
2020-04-22 15:38:57 +0500

    Add files via upload

--- WinWind/js/common.js
+++ WinWind/js/common.js
@@ -1,3 +1,19 @@
 $(function(){
+  $('.choice .btn').click(function () {
+    $('.choice__item').toggleClass('choice__item_hidden');
+  })
+
+
+  for (let i = 0; i < $('.moreover .item').length; i++) {
+
+    $('.moreover__button').eq(i).click(function() {
+      $('.moreover .item').eq(i).toggleClass('item_hidden');
+    });
+
+    $('.moreover .close').eq(i).click(function() {
+      $('.moreover .item').eq(i).addClass('item_hidden');
+    })
+
+  };
 
 });
