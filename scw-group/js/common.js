304405fb0d0060670a5c310df30fa2efa62c78b2
dmutokhlyaev <60260395+dmutokhlyaev@users.noreply.github.com>
2021-01-08 22:28:36 +0500

    Add files via upload

---
+++ scw-group/js/common.js
@@ -0,0 +1,13 @@
+$(function() {
+
+  /*
+  $(window).scroll(function () {
+    if($(this).scrollTop() > 50){
+      $('.header').addClass('header_sticky');
+    } else {
+      $('.header').removeClass('header_sticky');
+    }
+  }
+
+  */
+})
