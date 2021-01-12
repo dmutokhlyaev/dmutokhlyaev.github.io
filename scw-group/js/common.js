3fc1628ae2f6dd97a87ae5700fa1f6f2c03dac67
dmutokhlyaev <60260395+dmutokhlyaev@users.noreply.github.com>
2021-01-12 21:32:00 +0500

    Add files via upload

--- scw-group/js/common.js
+++ scw-group/js/common.js
@@ -84,4 +84,39 @@ $(function() {
   $('.user-menu__btn').click(function() {
     $('.user-menu__box').toggleClass('active');
   })
+
+  /* Выравниваение высоты ячеек на страницах услуг */
+
+  let priceNumberRows = $('.price__item').eq(0).find('.price__row').length;
+  let priceNumberColumn = $('.price__item').length;
+  
+  for(let i = 0; i < priceNumberRows; i++ ){
+    let maxHeight = 0;
+
+    for(let j = 0; j < priceNumberColumn; j++){
+      height = $('.price__item').eq(j).find('.price__row').eq(i).innerHeight();
+      if ( maxHeight < height ) {
+        maxHeight = height;
+      }
+    }
+
+    $('.price__item').each(function() {
+      $(this).find('.price__row').eq(i).innerHeight(maxHeight);
+    })
+  }
+
+  /* Фильтрация для доменных зон */
+
+  $('#domain__list').isotope({
+    itemSelector: '.domain__item',
+    layoutMode: 'fitRows'
+  });
+
+  $('#domain__filtr button').click(function(){
+    var filterValue = $(this).attr('data-filter');
+    $('#domain__list').isotope({ filter: filterValue });
+
+    $('#domain__filtr .active').removeClass('active');
+    $(this).addClass('active');
+  });
 })
